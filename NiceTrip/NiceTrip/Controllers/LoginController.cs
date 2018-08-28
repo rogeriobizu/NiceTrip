4ec1bd76f3019ca7423c5e0c6c1a78e1962d6c34
m013 <m013@benner.com.br>
2018-08-28 11:20:30 -0300

    Add pessoas Controller

--- NiceTrip/NiceTrip/Controllers/LoginController.cs
+++ NiceTrip/NiceTrip/Controllers/LoginController.cs
@@ -1,4 +1,5 @@
-﻿using NiceTrip.Classe;
+﻿
+using NiceTrip.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
---
+++ NiceTrip/NiceTrip/Models/Pessoa.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace NiceTrip.Models
+{
+    public class Pessoa
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Email { get; set; }
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+        public String Senha { get; set; }
+    }
+}
